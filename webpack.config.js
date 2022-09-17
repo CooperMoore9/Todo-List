fc673d29d4899a8c9dc5a6df2b10b8bc5b513711
CooperMoore9 <thefryman9@gmail.com>
2022-09-17 13:06:54 -0500

    Todo setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+      index: './src/index.ts',
+    },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
