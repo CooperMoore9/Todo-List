b9d506a5ee7a78db6734da3a44c427ea745cba26
CooperMoore9 <thefryman9@gmail.com>
2022-09-17 22:35:33 -0500

    General Layout of the site

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,15 @@
+"use strict";
 //Notes
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // -cog wheel on tasks header to change amount of rows
-
 // -Add new Projects and Tasks button will always be at bottom/end of list to add new objects
 // -Add new projects
-    // -When you click 'Add new Projects' just turn the 'Add new Projects' button to the new project, and move the add new project button down
-    // -Add new Tasks within projects
-
+// -When you click 'Add new Projects' just turn the 'Add new Projects' button to the new project, and move the add new project button down
+// -Add new Tasks within projects
 // -When different project is selected, present stored tasks
 // -New tasks will act similar, when you click 'Add new Task' button it turns the button into a new task and you can fill in the fields as necessary, also moves add new task button over
-
 // -add a bin button on the tasks in the bottom right to delete the tasks
 // -add a bin button the the right of projects to delete them (are you sure warning)
-
-import './style.css';
-import {itThisWorking} from './test'
-
-console.log(itThisWorking(1, 2));
+require("./style.css");
+const test_1 = require("./test");
+console.log((0, test_1.itThisWorking)(1, 2));
---
+++ dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.itThisWorking = void 0;
+function itThisWorking(x, y) {
+    return x + y;
+}
+exports.itThisWorking = itThisWorking;
