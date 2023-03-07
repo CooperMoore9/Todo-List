c578ab277603a3cbfcf020dd24316c9b7415a903
CooperMoore9 <thefryman9@gmail.com>
2023-03-07 13:15:48 -0600

    prettier

--- dist/addButtons.js
+++ dist/addButtons.js
@@ -3,45 +3,58 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.addTask = exports.addProject = exports.taskAddButton = void 0;
 const _1 = require(".");
 const uiUpdater_1 = require("./uiUpdater");
-let projectAddButton = document.querySelector('.addProject');
-exports.taskAddButton = document.querySelector('.addTask');
+let projectAddButton = document.querySelector(".addProject");
+exports.taskAddButton = document.querySelector(".addTask");
 const newDate = new Date(1995, 6, 2);
 function addProject() {
-    projectAddButton.addEventListener('click', () => {
-        if (_1.projectsSetup.projects[0]) {
-            let lastProjectNumber = parseInt(_1.projectsSetup.projects[_1.projectsSetup.projects.length - 1].name.replace(/^\D+/g, ''));
-            if (_1.projectsSetup.projects[_1.projectsSetup.projects.length - 1].name === `Project ${lastProjectNumber}`) {
-                _1.projectsSetup.addProject(`Project ${lastProjectNumber += 1}`);
-            }
-            else {
-                let numberOfProjects = 1;
-                _1.projectsSetup.projects.forEach(project => {
-                    numberOfProjects += 1;
-                });
-                _1.projectsSetup.addProject(`Project ${numberOfProjects}`);
-            }
-        }
-        else {
-            _1.projectsSetup.addProject(`Project 1`);
-        }
-        (0, uiUpdater_1.loopProjects)();
-    });
+  projectAddButton.addEventListener("click", () => {
+    if (_1.projectsSetup.projects[0]) {
+      let lastProjectNumber = parseInt(
+        _1.projectsSetup.projects[
+          _1.projectsSetup.projects.length - 1
+        ].name.replace(/^\D+/g, "")
+      );
+      if (
+        _1.projectsSetup.projects[_1.projectsSetup.projects.length - 1].name ===
+        `Project ${lastProjectNumber}`
+      ) {
+        _1.projectsSetup.addProject(`Project ${(lastProjectNumber += 1)}`);
+      } else {
+        let numberOfProjects = 1;
+        _1.projectsSetup.projects.forEach((project) => {
+          numberOfProjects += 1;
+        });
+        _1.projectsSetup.addProject(`Project ${numberOfProjects}`);
+      }
+    } else {
+      _1.projectsSetup.addProject(`Project 1`);
+    }
+    (0, uiUpdater_1.loopProjects)();
+  });
 }
 exports.addProject = addProject;
-;
 function addTask() {
-    exports.taskAddButton.addEventListener('click', () => {
-        if (_1.projectsSetup.projects[0]) {
-            let numOfTasks = 1;
-            _1.projectsSetup.projects[uiUpdater_1.projectIndex].tasks.forEach(task => {
-                numOfTasks += 1;
-            });
-            _1.projectsSetup.projects[uiUpdater_1.projectIndex].addTask(`Task ${numOfTasks}`, newDate, 'Description', false, 'None');
-            (0, uiUpdater_1.loopTasks)(_1.projectsSetup.projects[uiUpdater_1.projectIndex]);
+  exports.taskAddButton.addEventListener("click", () => {
+    if (_1.projectsSetup.projects[0]) {
+      let numOfTasks = 1;
+      _1.projectsSetup.projects[uiUpdater_1.projectIndex].tasks.forEach(
+        (task) => {
+          numOfTasks += 1;
         }
-        else {
-            return;
-        }
-    });
+      );
+      _1.projectsSetup.projects[uiUpdater_1.projectIndex].addTask(
+        `Task ${numOfTasks}`,
+        newDate,
+        "Description",
+        false,
+        "None"
+      );
+      (0, uiUpdater_1.loopTasks)(
+        _1.projectsSetup.projects[uiUpdater_1.projectIndex]
+      );
+    } else {
+      return;
+    }
+  });
 }
 exports.addTask = addTask;
--- dist/allProjectsObject.js
+++ dist/allProjectsObject.js
@@ -2,50 +2,39 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Task = exports.Project = exports.AllProjects = void 0;
 const uuid_1 = require("uuid");
-;
 class AllProjects {
-    constructor(projects) {
-        this.projects = projects;
-    }
-    ;
-    addProject(name) {
-        let newProject = new Project(name, []);
-        this.projects.push(newProject);
-    }
-    ;
-    logProjects() {
-        console.log(this);
-    }
-    ;
+  constructor(projects) {
+    this.projects = projects;
+  }
+  addProject(name) {
+    let newProject = new Project(name, []);
+    this.projects.push(newProject);
+  }
+  logProjects() {
+    console.log(this);
+  }
 }
 exports.AllProjects = AllProjects;
-;
-;
 class Project {
-    constructor(name, tasks) {
-        this.name = name;
-        this.tasks = tasks;
-        this.id = (0, uuid_1.v4)();
-    }
-    ;
-    addTask(title, dueDate, description, completed, severity) {
-        let newTask = new Task(title, dueDate, description, completed, severity);
-        this.tasks.push(newTask);
-    }
-    ;
+  constructor(name, tasks) {
+    this.name = name;
+    this.tasks = tasks;
+    this.id = (0, uuid_1.v4)();
+  }
+  addTask(title, dueDate, description, completed, severity) {
+    let newTask = new Task(title, dueDate, description, completed, severity);
+    this.tasks.push(newTask);
+  }
 }
 exports.Project = Project;
-;
 class Task {
-    constructor(title, dueDate, description, completed, severity) {
-        this.title = title;
-        this.dueDate = dueDate;
-        this.description = description;
-        this.id = (0, uuid_1.v4)();
-        this.completed = completed;
-        this.severity = severity;
-    }
-    ;
+  constructor(title, dueDate, description, completed, severity) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.id = (0, uuid_1.v4)();
+    this.completed = completed;
+    this.severity = severity;
+  }
 }
 exports.Task = Task;
-;
